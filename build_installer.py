92600e4cc7f5724a03944c799d57b3aa97775621
Nils Masen <nils@masen.name>
2009-09-17 19:38:06 +0200

    Updates to installer builder and removed a binary.

--- build_installer.py
+++ build_installer.py
@@ -24,7 +24,7 @@ rarpath = 'c:/Program files/WinRAR/rar.exe'
 # END Configuration ----------------------------------
 
 if __name__ == '__main__':
-    f = open('rel/lastbuild.txt','r')
+    f = open('inst/lastbuild.txt','r')
     old_build = f.read()
     f.close()
     new_build = int(old_build) + 1
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     print ' - Copying template...',
     build_date = date.today().strftime("%y%m%d")
     outfile = output.safe_substitute({'date': build_date, 'build': new_build})
-    shutil.copy('rel/'+template,'rel/'+outfile)
+    shutil.copy('inst/'+template,'rel/'+outfile)
     print 'OK'
     print ' - Adding files to installer...',
     cmdout = sp.Popen([rarpath,'u','-idq','-ep1','rel/'+outfile,'dist/*'], stdout=sp.PIPE).communicate()[0]
@@ -43,10 +43,10 @@ if __name__ == '__main__':
         sys.exit(2)
     print 'OK'
     print ' - Writing new state files...',
-    f = open('rel/lastbuild.txt','w')
+    f = open('inst/lastbuild.txt','w')
     f.write(str(new_build))
     f.close()
-    f = open('rel/lastrelease.txt','w')
+    f = open('inst/lastrelease.txt','w')
     f.write(outfile)
     f.close()
     print 'OK'
